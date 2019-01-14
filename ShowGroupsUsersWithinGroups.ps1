3c5a75e842478d8e2280014b81b9cabe31e8b065
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-15 12:39:19 +1300

    Update ShowGroupsUsersWithinGroups.ps1

--- ShowGroupsUsersWithinGroups.ps1
+++ ShowGroupsUsersWithinGroups.ps1
@@ -4,3 +4,10 @@ Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
 Clear-Host
 Write-Host "$GetAdGroupSingle"
 Write-Host "$GetAdGroupSingle_"
+
+#Get Multiple Groups and return mulitple members in those groups
+$ADGroups = Get-ADGroup -Filter {Name -like "Role-*"} | Select-Object Name
+ForEach-Object ($AdGroup in $ADGroups) {
+
+
+}
