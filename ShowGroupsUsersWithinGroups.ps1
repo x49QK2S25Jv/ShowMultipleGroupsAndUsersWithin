2efe29a986260348da288f0c46a40a67533830da
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-15 12:20:37 +1300

    Update ShowGroupsUsersWithinGroups.ps1

--- ShowGroupsUsersWithinGroups.ps1
+++ ShowGroupsUsersWithinGroups.ps1
@@ -1,1 +1,6 @@
-﻿Get-ADPrincipalGroupMembership
+﻿#Get members of a single group
+$GetAdGroupSingle = Read-Host
+Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
+Clear-Host
+Write-Host "$GetAdGroupSingle"
+Write-Host "$GetAdGroupSingle_"
