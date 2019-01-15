a2a84a0d50e6d045085ed3d0f4d04384d2b5b781
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-15 13:46:49 +1300

    Update ShowGroupsUsersWithinGroups.ps1

--- ShowGroupsUsersWithinGroups.ps1
+++ ShowGroupsUsersWithinGroups.ps1
@@ -1,13 +1,23 @@
-﻿#Get members of a single group
-$GetAdGroupSingle = Read-Host
-Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
-Clear-Host
-Write-Host "$GetAdGroupSingle"
-Write-Host "$GetAdGroupSingle_"
+﻿######################################################
+######################################################
+#Get members of a single group
+#$GetAdGroupSingle = Read-Host
+#Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
+#Clear-Host
+#Write-Host "$GetAdGroupSingle"
+#Write-Host "$GetAdGroupSingle_"
+#Completed############################################
+######################################################
+######################################################
+######################################################
 
 #Get Multiple Groups and return mulitple members in those groups
-$ADGroups = Get-ADGroup -Filter {Name -like "Role-*"} | Select-Object Name
-ForEach-Object ($AdGroup in $ADGroups) {
-
-
+Clear-Host
+Write-Host "Please enter the search group name e.g. Role-"
+$ADGroupLike = Read-Host
+ForEach ($AdGroup in $ADGroups) {
+Get-ADGroup -Filter *  -Identity $ADGroup
+Write-Host "$AdGroup"
+Write-Host ""
+Write-Host "==============" -ForegroundColor Red
 }
