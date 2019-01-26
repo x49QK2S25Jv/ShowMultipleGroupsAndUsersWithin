bf5862d0760a79635143fc57dd53412fe8b504de
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-26 21:10:17 +1300

    Update ShowGroupsUsersWithinGroups.ps1

--- ShowGroupsUsersWithinGroups.ps1
+++ ShowGroupsUsersWithinGroups.ps1
@@ -1,11 +1,11 @@
 ﻿######################################################
 ######################################################
-#Get members of a single group
-#$GetAdGroupSingle = Read-Host
-#Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
-#Clear-Host
-#Write-Host "$GetAdGroupSingle"
-#Write-Host "$GetAdGroupSingle_"
+Get members of a single group
+$GetAdGroupSingle = Read-Host
+Get-ADGroupMember -Identity $GetAdGroupSingle | Select-Object Name
+Clear-Host
+Write-Host "$GetAdGroupSingle"
+Write-Host "$GetAdGroupSingle_"
 #Completed############################################
 ######################################################
 ######################################################
@@ -13,15 +13,19 @@
 
 #Get Multiple Groups and return mulitple members in those groups
 Clear-Host
-Write-Host "Please enter the search group name e.g. Role-"
-$ADGroupSearch = Read-Host
-$ReturnedADGroups = Get-ADGroup -Filter * | where {$_.SamAccountName -like "*$ADGroupSearch*"} | Select-Object @{=Name,GroupCategory,Groupscope,ObjectClass
+$ADGroupSearch = Read-Host -Prompt "Please enter the search group name e.g. Role-"
+$ReturnedADGroups = Get-ADGroup -Filter * | Where {$_.SamAccountName -Like "*$ADGroupSearch*"} | Sort-Object | Select-Object Name,SamAccountName
 
 
-ForEach ($AdGroup in $ReturnedADGroups) {
+ForEach ($Group in $ReturnedADGroups) {
 Get-ADGroupMember -Identity "$AdGroup" #| where {$_.SamAccountName -like "$ADGroup"}
 Write-Host "$ReturnedADGroups"
-Write-Host "$AdGroup"
+Write-Host "$Group"
 Write-Host ""
 Write-Host "==============" -ForegroundColor Red
 }
+
+
+#Works
+Get-ADGroup -Filter * | Sort-Object | Select-Object Name,SamAccountName | where {$_.SamAccountName -like "*Role*"}
+
