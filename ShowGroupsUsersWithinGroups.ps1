99adf1d358d1c9d415e77638f09588ec81b246f8
x49QK2S25Jv <46139035+x49QK2S25Jv@users.noreply.github.com>
2019-01-29 21:47:25 +1300

    Update ShowGroupsUsersWithinGroups.ps1

--- ShowGroupsUsersWithinGroups.ps1
+++ ShowGroupsUsersWithinGroups.ps1
@@ -1,6 +1,7 @@
 ﻿$menu=@"
 1 Show members of a single group
 2 Search for all groups with a similar name
+3 Search for all groups with a similar name and export to csv
 Select a task by number or Q to quit
 "@
 
@@ -34,6 +35,31 @@ Write-Host "Members " -ForegroundColor Green
 Write-Output "$ADGroupMembers"
 Write-Host "==========" -ForegroundColor Red `n
 }
+        }
+"3" {
+Clear-Host
+$ADGroupSearch = Read-Host -Prompt "Please enter the search group name e.g. Role-"
+$ReturnedADGroups = Get-ADGroup -Filter * | Where {$_.SamAccountName -Like "*$ADGroupSearch*"}
+
+
+
+ForEach ($Group in $ReturnedADGroups) {
+$GroupNameFormatted = Get-ADGroup -Identity $Group | Select-Object Name -ExpandProperty Name
+Start-Sleep -Milliseconds 10
+Write-Host "Group Name " -NoNewline -ForegroundColor Green
+Write-Host  "$GroupNameFormatted"
+$ADGroupMembers = Get-ADGroupMember -Identity "$Group" | Select-Object Name -ExpandProperty Name | Out-String
+Write-Host "Members " -ForegroundColor Green
+Write-Output "$ADGroupMembers"
+Write-Host "==========" -ForegroundColor Red `n
+Read-Host -Prompt "Export to CSV? (y/n)"
+
+
+$KeyPress = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+
+$ExportSaveLocation = Read-Host -Prompt "Please enter the location to save the output"
+
         }
 default {
     Write-Host "I don't understand what you want to do." -ForegroundColor Yellow
